b4cb57e894b099f96cd5082424087bc0bcf12bde
hafeezzshs <hafeezz.dev@gmail.com>
2024-10-18 22:55:08 +0530

    add about me section and ubuntu font

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 import React from "react";
-import "./LandingPage.css"; // Import the CSS file
+import "./App.css";
 
 export default function LandingPage() {
   return (
     <div className="landing-container">
-      <div className="half-container dark-bg">
-        <div className="title-container">
-          <span className="mobile-title">web. dev.</span>
-          <span className="desktop-title">
+      <div className="hero-section">
+        <div className="hero-text">
+          <span className="mobile-text">web. dev.</span>
+          <span className="desktop-text">
             web. <br />
             dev.
           </span>
         </div>
       </div>
-      <div className="half-container">
-        <div className="content-container">
-          <h2 className="heading">about me</h2>
+      <div className="about-section">
+        <div className="about-content">
+          <h2 className="about-title">About me</h2>
           <p>
-            i&apos;m <strong>hafeezz</strong> <em>a.k.a.</em> <strong>Hassain</strong>,
+            I&apos;m <strong>Hafeezz</strong> <em>a.k.a.</em> <strong>Hassain Saheb</strong>,
             <br /> a passionate full-stack web developer driven by the love for creating seamless
-            digital experiences. with almost <strong>1.5 years</strong> of hands-on experience, i
+            digital experiences. With almost <strong>1.5 years</strong> of hands-on experience, I
             focus on building modern, efficient, and user-friendly web applications using
             cutting-edge technologies.
           </p>
-          <p className="paragraph">
-            i continually hone my skills to deliver <strong>high-quality</strong> solutions. my
-            expertise includes <strong>react</strong>, <strong>next.js</strong>,{" "}
-            <strong>tailwind</strong>, <strong>node.js</strong>, <strong>angular</strong>, and{" "}
-            <strong>mongodb</strong>. i thrive in collaborative environments, enjoying the
+          <p className="about-description">
+            I continually hone my skills to deliver <strong>high-quality</strong> solutions. My
+            expertise includes <strong>React</strong>, <strong>Next.js</strong>,{" "}
+            <strong>Tailwind</strong>, <strong>Node.js</strong>, <strong>Angular</strong>, and{" "}
+            <strong>Mongodb</strong>. I thrive in collaborative environments, enjoying the
             opportunity to learn from others while also sharing my knowledge and experiences.
           </p>
         </div>
@@ -35,4 +35,3 @@ export default function LandingPage() {
     </div>
   );
 }
-
